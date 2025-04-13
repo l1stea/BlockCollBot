30dc4cd14dfda9518cb98a4bba91b0ee4e69b863
Elisey Timoshenko <elisei.timoshenko@yandex.ru>
2025-04-13 10:59:26 +0700

    Убраны принты

--- handler.py
+++ handler.py
@@ -33,13 +33,9 @@ def handle_add_client(text):
     try:
         # Сначала делим по первому пробелу
         _, rest = text.split(" ", 1)
-        print(_)
-        print(rest)
         # Теперь делим оставшееся по запятым
         first_name, last_name, email, phone_number, address = rest.split(",", 4)
         add_client(first_name.strip(), last_name.strip(), email.strip(), phone_number.strip(), address.strip())
-        print(first_name.strip())
-        print(address.strip())
         return "Клиент добавлен!"
     except ValueError:
         return "Ошибка: команда должна быть в формате '/addclient <first_name>, <last_name>, <email>, <phone_number>, <address>'."
